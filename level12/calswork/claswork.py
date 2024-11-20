f9c0f0a8a20bfe51b16c7dc5fb538fc7760b1c32
giorgikenchoshvili <gkenchoshvili63@gmail.com>
2024-11-20 19:22:16 +0400

    commit

---
+++ level12/calswork/claswork.py
@@ -0,0 +1,4 @@
+names={"giorgi","sofo","giga","vano","barbare","tazo","erekle","mariami","gaga"}
+
+number=int(input("enter number"))
+print(names{number})
---
+++ levle04/homework/homework.py
@@ -0,0 +1,13 @@
+name=(input("plss eenter your name"))
+
+friend=(input("pls enter your friend "))
+
+print(name) 
+print(friend)
+
+
+age=int(input("pls enter your age :"))
+
+print(age+10)
+
+
