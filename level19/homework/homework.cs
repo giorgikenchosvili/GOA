19f48537a1fc6204ad31707511c0353ed1a7d491
giorgikenchoshvili <gkenchoshvili63@gmail.com>
2025-01-11 16:20:45 +0400

    commit

---
+++ level19/homework/homework.cs
@@ -0,0 +1,15 @@
+body {
+  font-family: Arial, sans-serif;
+  background-color: red;
+  color: white;
+}
+h1 {
+  text-align: center;
+  color: yellow;
+}
+.food h2 {
+  color: blue;
+}
+.food p {
+  color: green;
+}
